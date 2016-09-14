bd80a8d2fb35e13a937b7d6370962974357df6ba
smikodanic <smikodanic@gmail.com>
2016-09-14 17:43:15 +0200

    hash auth - add hash string only to API URLs

--- src/factory/hashInterceptApiRequest.js
+++ src/factory/hashInterceptApiRequest.js
@@ -3,7 +3,7 @@
  * Notice: $injector is required to inject hashAuth, because config() accepts providers only not services.
  */
 
-module.exports = function ($injector) {
+module.exports = function ($injector, NGPASSPORT_CONF_HASH) {
     'use strict';
 
     var interceptApiRequest = {};
@@ -20,13 +20,13 @@ module.exports = function ($injector) {
 
         //Intercept by adding hash string at the URL's end, only when cookie is set, e.g. when user is logged in.
         //When user is not logged in don't add hash sufix at the URL's end.
+        // Important: only to API endpoints will be added hash string e.g. to urls which have NGPASSPORT_CONF_HASH.API_BASE_URL
         var hashStr = hashAuth.getCookie('authAPI').hash;
-        console.log('hashStr ', hashStr);
-        if (hashStr) {
+        if (hashStr && config.url.indexOf(NGPASSPORT_CONF_HASH.API_BASE_URL) !== -1) {
             config.url = config.url + '/' + hashStr;
         }
 
-        console.log('$http config\n', JSON.stringify(config, null, 2));
+        // console.log('$http config\n', JSON.stringify(config, null, 2));
 
         return config;
     };
